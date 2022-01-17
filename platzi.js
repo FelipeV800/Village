cc18de36e35e9553db5118f382105bf5629f1047
Felipe Vasquez <vasquezruizcarlos@gmail.com>
2022-01-17 10:55:00 -0500

    se le dio la movilidad al personaje y los limites del mapa, ademas se guarda la posicion de los animales cada vez que se redibujan

--- platzi.js
+++ platzi.js
@@ -1,6 +1,6 @@
 var texto = document.getElementById('textmaxvacas');
 var boton = document.getElementById("btngenerar");
-boton.addEventListener("click", dibujar);
+boton.addEventListener("click", datosAnimales);
 
 var vp = document.getElementById("villakitsu");
 var papel = vp.getContext("2d");
@@ -10,11 +10,24 @@ document.addEventListener("keydown", mover);
 var xPj = 200;
 var yPj = 250;
 
+var xVaca = new Array();
+var yVaca = new Array();
+
+var xCerdo = new Array();
+var yCerdo = new Array();
+
+var xPollo = new Array();
+var yPollo = new Array();
+
 var teclas = {
     LEFT: 37,
     UP: 38,
     RIGHT: 39,
-    DOWN: 40
+    DOWN: 40,
+    A: 65,
+    W: 87,
+    D: 68,
+    S: 83
 };
 
 var fondo = {
@@ -67,55 +80,78 @@ function cargarFondo() {
     dibujar();
 }
 
-function cargarVaca() {
-    vaca.cargaOK = true;
-    dibujar();
-}
-
-function cargarCerdo() {
-    cerdo.cargaOK = true;
-    dibujar();
-}
-
-function cargarPollo() {
-    pollo.cargaOK = true;
-    dibujar();
-}
-
 function cargarPersonaje() {
     personaje.cargaOK = true;
     dibujar();
 }
 
-function dibujar() {
-    var cantidad = random(1, parseInt(texto.value));
+function cargarVaca() {
+    vaca.cargaOK = true;
+    datosAnimales();
+}
 
+function cargarCerdo() {
+    cerdo.cargaOK = true;
+    datosAnimales();
+}
+
+function cargarPollo() {
+    pollo.cargaOK = true;
+    datosAnimales();
+}
+
+function datosAnimales() {
+    if (vaca.cargaOK) {
+        var cantidad = random(1, parseInt(texto.value));
+        for (var i = 0; i < cantidad; i++) {
+            var x = (random(0, 7)) * 60;
+            var y = (random(0, 7)) * 60;
+            xVaca[i] = x;
+            yVaca[i] = y;
+        }
+    }
+
+    if (cerdo.cargaOK) {
+        for (var i = 0; i < random(1, 3); i++) {
+            var x = (random(0, 7)) * 60;
+            var y = (random(0, 7)) * 60;
+            xCerdo[i] = x;
+            yCerdo[i] = y;
+        }
+    }
+
+    if (pollo.cargaOK) {
+        for (var i = 0; i < random(0, 2); i++) {
+            var x = (random(0, 7)) * 60;
+            var y = (random(0, 7)) * 60;
+            xPollo[i] = x;
+            yPollo[i] = y;
+        }
+    }
+    dibujar();
+}
+
+function dibujar() {
     if (fondo.cargaOK) {
         papel.drawImage(fondo.imagen, 0, 0);
     }
 
     if (vaca.cargaOK) {
-        for (var v = 0; v < cantidad; v++) {
-            var x = (random(0, 7)) * 60;
-            var y = (random(0, 7)) * 60;
-            papel.drawImage(vaca.imagen, x, y);
-        }
+        for(var i=0; i<10; i++){
+			papel.drawImage(vaca.imagen, xVaca[i], yVaca[i]);		
+		}
     }
 
     if (cerdo.cargaOK) {
-        for (var v = 0; v < random(1, 3); v++) {
-            var x = (random(0, 7)) * 60;
-            var y = (random(0, 7)) * 60;
-            papel.drawImage(cerdo.imagen, x, y);
-        }
+        for(var i=0; i<10; i++){
+			papel.drawImage(cerdo.imagen, xCerdo[i], yCerdo[i]);		
+		}
     }
 
     if (pollo.cargaOK) {
-        for (var v = 0; v < random(0, 2); v++) {
-            var x = (random(0, 7)) * 60;
-            var y = (random(0, 7)) * 60;
-            papel.drawImage(pollo.imagen, x, y);
-        }
+        for(var i=0; i<10; i++){
+			papel.drawImage(pollo.imagen, xPollo[i], yPollo[i]);		
+		}
     }
 
     if (personaje.cargaOK) {
@@ -124,20 +160,74 @@ function dibujar() {
 }
 
 function mover(properties) {
-    var movimiento = 10;
+    var movimiento = 20;
     switch (properties.keyCode) {
         case teclas.UP:
-            yPj = yPj - movimiento;
-            dibujar(xPj, yPj);
+            if (yPj < 10) {
+                alert("Creo que aun no podemos ir ahi!!");
+            } else {
+                yPj = yPj - movimiento;
+                dibujar(xPj, yPj);
+            }
             break;
         case teclas.DOWN:
-            
+            if (yPj > 410) {
+                alert("Creo que aun no podemos ir ahi!!");
+            } else {
+                yPj = yPj + movimiento;
+                dibujar(xPj, yPj);
+            }
             break;
         case teclas.LEFT:
-
+            if (xPj < 0) {
+                alert("Creo que aun no podemos ir ahi!!");
+            } else {
+                xPj = xPj - movimiento;
+                dibujar(xPj, yPj);
+            }
             break;
         case teclas.RIGHT:
-            
+            if (xPj > 430) {
+                alert("Creo que aun no podemos ir ahi!!");
+            } else {
+                xPj = xPj + movimiento;
+                dibujar(xPj, yPj);
+            }
+            break;
+    }
+
+    switch (properties.keyCode) {
+        case teclas.W:
+            if (yPj < 10) {
+                alert("Creo que aun no podemos ir ahi!!");
+            } else {
+                yPj = yPj - movimiento;
+                dibujar(xPj, yPj);
+            }
+            break;
+        case teclas.S:
+            if (yPj > 410) {
+                alert("Creo que aun no podemos ir ahi!!");
+            } else {
+                yPj = yPj + movimiento;
+                dibujar(xPj, yPj);
+            }
+            break;
+        case teclas.A:
+            if (xPj < 0) {
+                alert("Creo que aun no podemos ir ahi!!");
+            } else {
+                xPj = xPj - movimiento;
+                dibujar(xPj, yPj);
+            }
+            break;
+        case teclas.D:
+            if (xPj > 430) {
+                alert("Creo que aun no podemos ir ahi!!");
+            } else {
+                xPj = xPj + movimiento;
+                dibujar(xPj, yPj);
+            }
             break;
     }
 }
